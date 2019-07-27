01e7655a96bd5e853715f3eaf45862097513c1c5
BeYkeRYkt <beykerykt@gmail.com>
2019-07-27 22:15:13 +0900

    marmite: set keystore value

--- omni_marmite.mk
+++ omni_marmite.mk
@@ -34,6 +34,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libcryptfs_hw
 
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.hardware.keystore=msm8937
+
 ## Device identifier. This must come after all inclusions
 PRODUCT_NAME := omni_marmite
 PRODUCT_DEVICE := marmite
